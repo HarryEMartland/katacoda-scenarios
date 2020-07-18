07ec111a9bc4a55bbc47038403cf9e8d4b7f93eb
martlandh <HarryMartland@Rentalcars.com>
2020-07-18 13:45:39 +0100

    changed to use gist for deployment

--- deleting-k8s-pods/setup.sh
+++ deleting-k8s-pods/setup.sh
@@ -3,4 +3,9 @@
 apt-get update
 apt-get install -y apache2-utils
 
-kubectl apply -f /usr/local/bin/app
+until kubectl get pods
+do
+  echo "Waiting for k8s"
+done
+
+kubectl apply -f https://gist.githubusercontent.com/HarryEMartland/98b3c2bcd89b07ef5e555085179ed7c8/raw/7594a6d4153c5d0489f18f0811b7a7d967cf2287/load-app-deployment.yaml
