323337c8595247ddb81b3355f0589e824a5bf301
martlandh <HarryMartland@Rentalcars.com>
2020-07-20 18:45:47 +0100

    silence commands

--- deleting-k8s-pods/assets/setup.sh
+++ deleting-k8s-pods/assets/setup.sh
@@ -3,18 +3,18 @@
 setup()
 {
 echo "Installing Tools"
-apt-get update
-apt-get install -y libssl-dev < "/dev/null"
-git clone https://github.com/giltene/wrk2.git;
+apt-get update &> /dev/null
+apt-get install -y libssl-dev &> /dev/null
+git clone https://github.com/giltene/wrk2.git &> /dev/null;
 cd wrk2
-make
+make &> /dev/null
 # move the executable to somewhere in your PATH
 cp wrk /usr/local/bin
 
 echo "Starting Minikube"
-minikube start
+minikube start &> /dev/null
 echo "Enabling ingress"
-minikube addons enable ingress
+minikube addons enable ingress &> /dev/null
 
 echo "Deploying App"
 kubectl apply -f https://gist.githubusercontent.com/HarryEMartland/98b3c2bcd89b07ef5e555085179ed7c8/raw/7594a6d4153c5d0489f18f0811b7a7d967cf2287/load-app-deployment.yaml
