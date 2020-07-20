1964d0cac30c264e4a427d0cf34c7e947bd65b4c
martlandh <HarryMartland@Rentalcars.com>
2020-07-20 18:16:21 +0100

    changed to minikube

--- deleting-k8s-pods/setup.sh
+++ deleting-k8s-pods/setup.sh
@@ -1,4 +1,7 @@
 
+alias k=kubectl
+
+echo "Installing Tools"
 apt-get update
 apt-get install -y build-essential libssl-dev git zlib1g-dev
 git clone https://github.com/giltene/wrk2.git
@@ -7,10 +10,18 @@ make
 # move the executable to somewhere in your PATH
 cp wrk /usr/local/bin
 
-until kubectl get pods
-do
-  echo "Waiting for k8s"
-  sleep 1
-done
+echo "Starting Minikube"
+minikube start
+echo "Enabling ingress"
+minikube addons enable ingress
 
+echo "Deploying App"
 kubectl apply -f https://gist.githubusercontent.com/HarryEMartland/98b3c2bcd89b07ef5e555085179ed7c8/raw/7594a6d4153c5d0489f18f0811b7a7d967cf2287/load-app-deployment.yaml
+
+echo "Waiting for app"
+
+kubectl wait --for=condition=available deployment/load-app
+
+echo "Finished"
+
+kubectl get pods
