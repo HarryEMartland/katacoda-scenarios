417b940dab388fd74d18a2b56461291bd0e30cab
Harry Martland <HarryEMartland@gmail.com>
2021-01-30 16:49:39 +0000

    checkout kata-bootstraps in temp dir and copy to wd

--- kata-bootstraps-js-mocha/setup.sh
+++ kata-bootstraps-js-mocha/setup.sh
@@ -1,1 +1,3 @@
-git clone https://github.com/swkBerlin/kata-bootstraps.git;
+git clone https://github.com/swkBerlin/kata-bootstraps.git /tmp/kata-bootstraps;
+
+cp -r /tmp/kata-bootstraps/js/mocha-tests/* ./;
