e29ef989e3b766c8ade1ef089e363417c0b2cb68
Harry Martland <HarryEMartland@gmail.com>
2021-01-30 17:25:28 +0000

    fixes copying hidden files

--- kata-bootstraps-js-mocha/setup.sh
+++ kata-bootstraps-js-mocha/setup.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/swkBerlin/kata-bootstraps.git /tmp/kata-bootstraps;
 
-cp -r /tmp/kata-bootstraps/js/mocha-tests/* ./;
+cp -r /tmp/kata-bootstraps/js/mocha-tests/. ./;
 
 npm install;
 
